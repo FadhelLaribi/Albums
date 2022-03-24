7cfbf7a5cb9494da5ae16027317fad63b58ca9b3
Fadhel Laribi <fadhel@lepotcommun.fr>
2022-03-24 18:44:23 +0100

    Add application class and set up timber

---
+++ app/src/main/java/fr/lbc/albums/application/MyApplication.kt
@@ -0,0 +1,13 @@
+package fr.lbc.albums.application
+
+import android.app.Application
+import timber.log.Timber
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
